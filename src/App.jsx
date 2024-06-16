791c50ca550c378a941a372db564691159e0ce0d
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2024-06-16 11:38:08 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Firstsection from './Components/Firstsection';
+import Navbar from './Components/Navbar';
+import Secondsection from './Components/Secondsection';
+import Thirdsection from './Components/Thirdsection';
+// import { Route,Router,Routes } from 'react-router-dom'  
+function App() {
+
+
+  return (
+    <div >
+    <p>HEY</p>
+    </div>
+  )
+}
+
+export default App
